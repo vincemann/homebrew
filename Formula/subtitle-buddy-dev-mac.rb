4d4cde638ef518747d27afebc84bfa865c512e8b
vincemann <v.conrad98@web.de>
2024-05-14 15:37:39 +0200

    update dev mac hash

--- Formula/subtitle-buddy-dev-mac.rb
+++ Formula/subtitle-buddy-dev-mac.rb
@@ -2,7 +2,7 @@ class SubtitleBuddyDevMac < Formula
   desc "Opensource Subtitle Player"
   homepage "https://github.com/vincemann/subtitle-buddy"
   url "http://192.168.178.69:8000/subtitle-buddy-1.1.0-macx64.tar.gz"
-  sha256 "a45eb9f1f860009df7aaeefda4d54d1b0fbf64f0246de40ef6028f86e0dad538"
+  sha256 "7f0aa375c5ecdd581279e79d256245e753bee74e86b2d8bbcda1bb494e66f879"
 
   def install
     # Extract the tarball to the libexec directory
