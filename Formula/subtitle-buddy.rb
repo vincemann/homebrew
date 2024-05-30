f4f4c1676dc15807ddd19edd6e9f151e06f301d2
vincemann <v.conrad98@web.de>
2024-05-30 15:24:50 +0200

    update file names

--- Formula/subtitle-buddy.rb
+++ Formula/subtitle-buddy.rb
@@ -3,10 +3,10 @@ class SubtitleBuddy < Formula
   homepage "https://github.com/vincemann/subtitle-buddy"
 
    if OS.mac?
-    url "http://192.168.178.69:8000/subtitle-buddy-1.1.0-macx64.tar.gz"
+    url "http://192.168.178.69:8000/image-mac.zip"
     sha256 "570fab7d3c231bd03e4998666b533f890ca64c52a4f2de04eebb953243e3c4b9"
   elsif OS.linux?
-    url "http://192.168.178.69:8000/subtitle-buddy-1.1.0-linuxx64.tar.gz"
+    url "http://192.168.178.69:8000/image-linux.zip"
     sha256 "05aeb52e7fe5e09c1fc37face70a061381cb016eac99b79e5837fab4f82c0596"
   end
 
