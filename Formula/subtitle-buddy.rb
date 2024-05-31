d83700c1a2d00a0f92dd8ccde6d9793a98c5ee56
vincemann <v.conrad98@web.de>
2024-05-31 15:40:24 +0200

    replace formular wget downloads with curl

--- Formula/subtitle-buddy.rb
+++ Formula/subtitle-buddy.rb
@@ -5,7 +5,7 @@ class SubtitleBuddy < Formula
 
   if OS.mac?
     url "#{download_host}/image-mac.zip"
-    sha256 "5e39704e417be7be6dba6afe7fc3201cad9c01391b1e3407cbddcdcd85a94cbd"
+    sha256 "1ed7f332ed7c839a99d43c88066b448e914c09a9acd0db339fe3d3fa06e85311"
   elsif OS.linux?
     url "#{download_host}/image-linux.zip"
     sha256 "05aeb52e7fe5e09c1fc37face70a061381cb016eac99b79e5837fab4f82c0596"
@@ -20,19 +20,19 @@ class SubtitleBuddy < Formula
   end
 
   def post_install
-    if OS.isMac?
-      # homebrew corrupts some files, so I need to manually replace them...
+    if OS.mac?
+      # URLs to the correct libraries
       download_host = "http://192.168.178.69:8000"
       jnativehook_arm64_url = "#{download_host}/libJNativeHook_arm64.dylib"
       jnativehook_x86_64_url = "#{download_host}/libJNativeHook_x86_64.dylib"
       libjvm_url = "#{download_host}/libjvm.dylib"
       libjsig_url = "#{download_host}/libjsig.dylib"
 
-      # Download and replace the necessary files
-      system "wget", "-O", "#{libexec}/lib/jnativehook/darwin/arm64/libJNativeHook.dylib", jnativehook_arm64_url
-      system "wget", "-O", "#{libexec}/lib/jnativehook/darwin/x86_64/libJNativeHook.dylib", jnativehook_x86_64_url
-      system "wget", "-O", "#{libexec}/lib/server/libjvm.dylib", libjvm_url
-      system "wget", "-O", "#{libexec}/lib/server/libjsig.dylib", libjsig_url
+      # Download and replace the necessary files using curl
+      system "curl", "-L", "-o", "#{libexec}/lib/jnativehook/darwin/arm64/libJNativeHook.dylib", jnativehook_arm64_url
+      system "curl", "-L", "-o", "#{libexec}/lib/jnativehook/darwin/x86_64/libJNativeHook.dylib", jnativehook_x86_64_url
+      system "curl", "-L", "-o", "#{libexec}/lib/server/libjvm.dylib", libjvm_url
+      system "curl", "-L", "-o", "#{libexec}/lib/server/libjsig.dylib", libjsig_url
 
       # Ensure the files have the correct permissions
       chmod 0755, "#{libexec}/lib/jnativehook/darwin/arm64/libJNativeHook.dylib"
