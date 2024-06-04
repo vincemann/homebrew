0721e2b419a3eda69705522ed8fa69b5c42718cc
vincemann <v.conrad98@web.de>
2024-06-04 17:48:37 +0200

    update hash

--- Formula/subtitle-buddy.rb
+++ Formula/subtitle-buddy.rb
@@ -8,7 +8,7 @@ class SubtitleBuddy < Formula
     sha256 "15d21e55819d41e042b700f67e951b81340988b01bce24f05216aeed81015ce1"
   elsif OS.linux?
     url "#{download_host}/image-linux.zip"
-    sha256 "d3129042997423bcecd1fc1b907947c8f7408255929be77a69aec981dd49b046"
+    sha256 "a4bc36527e3239b3a19e3188063b5ec44f4ac2f250b51df2ed8d21691defa1ec"
   end
 
   def install
